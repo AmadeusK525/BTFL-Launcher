4ff3f9d29b4b46ccdf5492baef7f298a6541513b
AmadeusK525 <bronstein.franceschetti@protonmail.com>
2021-04-08 00:19:37 -0300

    Started handling the background myself, so now resizing is super smooth :)

--- src/OverviewPanel.cpp
+++ src/OverviewPanel.cpp
@@ -1,22 +1,27 @@
 #include "OverviewPanel.h"
 
-BEGIN_EVENT_TABLE(OverviewPanel, wxCustomBackgroundWindow<wxPanel>)
+#include <wx\dcbuffer.h>
+#include <wx\dcgraph.h>
+
+BEGIN_EVENT_TABLE(OverviewPanel, wxPanel)
 EVT_SIZE(OverviewPanel::OnSize)
+EVT_PAINT(OverviewPanel::OnPaint)
 END_EVENT_TABLE()
 
-OverviewPanel::OverviewPanel(wxWindow* parent) {
-	Create(parent);
-	
-	wxBitmap bmp(wxBITMAP_PNG(pabloRA-Phoenix));
-	m_background = bmp.ConvertToImage();
-	m_bgRatio = (double)m_background.GetWidth() / m_background.GetHeight();
-	bmp = m_background;
-
-	SetBackgroundBitmap(bmp);
+OverviewPanel::OverviewPanel(wxWindow* parent) : wxPanel(parent) {
+	m_background = wxBITMAP_PNG(pabloRA-Phoenix);
+	SetBackgroundColour(wxColour(10, 10, 10));
 }
 
 void OverviewPanel::OnSize(wxSizeEvent& event) {
-	wxSize size = GetVirtualSize();
-	SetBackgroundBitmap(amGetScaledImage(size.x, size.y, m_background, m_bgRatio));
+	wxSize size = GetClientSize();
+	m_scale = (double)size.x / m_background.GetWidth();
+	m_bgy = (size.y - ((double)m_background.GetHeight() * m_scale)) / 2;
 	Refresh();
+}
+
+void OverviewPanel::OnPaint(wxPaintEvent& event) {
+	wxPaintDC dc(this);
+	dc.SetUserScale(m_scale, m_scale);
+	dc.DrawBitmap(m_background, 0, m_bgy / m_scale);
 }
